aea281ca7b1276cfe9e8a2152d879eb251ab8df7
Jiya-Singh05 <scholarjiyasingh@gmail.com>
2025-08-27 01:15:07 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a,b,sum;
+    printf("Enter your first number\n");
+    scanf("%d",&a);
+    printf("Enter your second number\n");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The sum of two numbers is %d",sum);
+    return 0;
+
+}
