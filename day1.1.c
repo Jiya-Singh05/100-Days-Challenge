01eb1c534909bc8a206c4bf35bcd96e41eb32f19
Jiya-Singh05 <scholarjiyasingh@gmail.com>
2025-09-05 18:06:28 +0000

    Add files via upload

---
+++ day1.1.c
@@ -0,0 +1,17 @@
+//write a program to input numbers and display there sum
+#include <stdio.h>
+int main() {
+int a,b,sum;
+printf("enter two numbers:");
+scanf("%d%d", &a,&b);
+sum=a+b;
+printf("the sum is:%d\n",sum);
+
+
+
+
+
+
+return 0;
+}
+
