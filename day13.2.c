bc1fd4be0f96bc607ee802f14016420c32db62d4
Jiya Singh <scholarjiyasingh@gmail.com>
2025-09-09 00:09:52 +0530

    Add files via upload

---
+++ day13.2.c
@@ -0,0 +1,18 @@
+//Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+int main() {
+    
+int a,n;
+    
+printf("Enter the number till which you want to print the sequence: ");    
+scanf("%d",&n);
+
+for(a=1;a<=n;a++)
+printf(" %d",a);
+    
+
+    
+    
+return 0;
+}
