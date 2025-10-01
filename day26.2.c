1a0d293edf48a217bac2c73866326a1231c894fd
Jiya Singh <scholarjiyasingh@gmail.com>
2025-10-01 19:25:46 +0530

    Add files via upload

---
+++ day26.2.c
@@ -0,0 +1,35 @@
+//Write a program to print the following pattern:
+
+#include <stdio.h>
+int main() {
+    
+int i,j,k;
+
+printf("*\n\n");
+
+for(i=1;i<=3;i++)
+{
+    printf("*\n");
+}
+
+printf("\n");
+
+for(j=1;j<=5;j++)
+{
+    printf("*\n");
+}
+
+printf("\n");
+
+for(k=1;k<=3;k++)
+{
+    printf("*\n");
+}    
+ 
+ printf("\n");
+ 
+ printf("*");  
+    
+    
+    return 0;
+}
