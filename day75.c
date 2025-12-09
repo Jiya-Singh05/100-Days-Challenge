ac918bf5842a3afae0e001879b3dcb7cd6d40fd8
Jiya Singh <scholarjiyasingh@gmail.com>
2025-12-09 17:09:11 +0530

    Add files via upload

---
+++ day75.c
@@ -0,0 +1,24 @@
+//Open an existing file in append mode and allow the user to enter a new line of text. Append the text at the end without overwriting existing content.
+
+
+#include <stdio.h>
+int main() 
+{
+    FILE *fp;
+    char filename[150];
+    char text[250];
+    printf("Enter the file name: ");
+    scanf("%s", filename);
+    getchar();
+    fp = fopen(filename, "a");
+    if (fp == NULL) 
+    { printf("Could not open file.\n");
+        return 1;
+    }
+    printf("Enter text to append: ");
+    fgets(text, sizeof(text), stdin);
+    fputs(text, fp);
+    fclose(fp);
+    printf("Text appended to %s\n", filename);
+    return 0;
+}
